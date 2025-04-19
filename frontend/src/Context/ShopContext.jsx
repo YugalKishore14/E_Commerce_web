1ec0620303553469f9c6f01bf0c9877f33fd51a2
yugaldhiman <yugaldhiman14@gmail.com>
2025-04-19 09:21:03 +0530

    add allProducts Api

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,23 +1,28 @@
-import React, { createContext, useState } from "react";
-import all_product from "../Component/Assest/all_product";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ShopContext = createContext(null);
 
 const getDefaultcart = () => {
     let cart = {};
-    for (let index = 0; index < all_product.length + 1; index++) {
+    for (let index = 0; index < 300 + 1; index++) {
         cart[index] = 0;
     }
     return cart;
 };
 
 const ShopContextProvider = (props) => {
+    const [all_product, setAll_product] = useState([]);   //
     const [cartItems, setCartItems] = useState(getDefaultcart());
+
+    useEffect(() => {
+        fetch('https://e-commerce-backend-sme3.onrender.com/allproducts')
+            .then((Response) => Response.json())
+            .then((data) => setAll_product(data))
+    }, [])
 
     const addTocart = (itemId) => {
         setCartItems((prev) => {
             const updatedCart = { ...prev, [itemId]: prev[itemId] + 1 };
-            // console.log( updatedCart); // Updated cart yahan dikhega
             return updatedCart;
         });
     };
@@ -25,39 +30,35 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => {
             const updatedCart = { ...prev, [itemId]: prev[itemId] - 1 };
-      // Updated cart yahan dikhega
+            // Updated cart yahan dikhega
             return updatedCart;
         });
     };
 
-    const getTotalPrice = ()=>{
-     let  totalPrice = 0;
-     for(let item in cartItems)
-     {
-        if(cartItems[item]>0)
-        {
-            let itemInfo = all_product.find(product=>product.id===Number(item))
-             totalPrice += itemInfo.new_price * cartItems[item]
-          
+    const getTotalPrice = () => {
+        let totalPrice = 0;
+        for (let item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find(product => product.id === Number(item))
+                totalPrice += itemInfo.new_price * cartItems[item]
+
+            }
         }
-     }
-     return totalPrice;
-     
+        return totalPrice;
+
     }
-    const getTotalItem=()=>{
+    const getTotalItem = () => {
         let totalItem = 0;
-        for (const item in cartItems)
-        {
-          if(cartItems[item]>0)
-          {
-            totalItem += cartItems[item]
-          }
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                totalItem += cartItems[item]
+            }
         }
         return totalItem
 
     }
 
-    const contextValue = {getTotalItem,getTotalPrice,all_product,cartItems, addTocart, removeFromCart};
+    const contextValue = { getTotalItem, getTotalPrice, all_product, cartItems, addTocart, removeFromCart };
 
     return (
         <ShopContext.Provider value={contextValue}>
