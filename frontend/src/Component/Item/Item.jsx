74b0223e30f4fc22eb87f53353777ab85da546b1
yugaldhiman <yugaldhiman14@gmail.com>
2025-04-19 09:07:54 +0530

    change symbol icon

--- frontend/src/Component/Item/Item.jsx
+++ frontend/src/Component/Item/Item.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom'
 function Item(props) {
   return (
     <div className="item">
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.img} alt="" /></Link>
-        <p>{props.name}</p>
-        <div className="item-price">
-            <div className="item-old-price">
-                $ {props.old_price}
-            </div>
-            <div className="item-new-price">
-                $ {props.new_price}
-            </div>
+      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0, 0)} src={props.img} alt="" /></Link>
+      <p>{props.name}</p>
+      <div className="item-price">
+        <div className="item-old-price">
+          ₹ {props.old_price}
         </div>
+        <div className="item-new-price">
+          ₹ {props.new_price}
+        </div>
+      </div>
     </div>
   )
 }
